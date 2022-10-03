14cb126a55fb79c742bc754a3d9b52611c098cfb
mathiasme <38974495+Mathiasme@users.noreply.github.com>
2022-10-03 21:55:11 +0200

    added class for hover + id night mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
       <div className="top">
         <a href="https://ranch.finance/"><img src="https://www.ranch.finance/static/media/logo/hat.png" alt="Logo" width="100" height="100"/></a>
-        <div id="logo-wrapper" className="text-logo"><a href="https://ranch.finance/"> <img src="https://www.ranch.finance/static/media/logo/ranch.finance_transparent.svg" alt="Logo-text"/></a></div>
+        <div id="logo-wrapper" className="text-logo"><a href="https://ranch.finance/"> <img id="rondNight" class="rond"  src="https://www.ranch.finance/static/media/logo/ranch.finance_transparent.svg" alt="Logo-text"/></a></div>
       </div>
   );
 }
